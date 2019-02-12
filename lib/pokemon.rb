e4efcbce9ab3296d14f65ea86d4eecfd6bae116e
Edgar Irizarry <eirizarry15@yahoo.com>
2019-02-12 15:44:57 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -5,3 +5,12 @@ class Pokemon
 
 end
 
+def self.save(name, type, db)
+  db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name. type)
+end 
+
+def self.find(num, db)
+  pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", [num], [self.id])
+  self.hp = num 
+ end 
+end 
